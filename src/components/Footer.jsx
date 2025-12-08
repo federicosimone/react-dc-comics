d7dce1f512601e70ad9eeb2275ec8f75b9d82ac0
federicosimone <federico.simone15@gmail.com>
2025-12-08 11:21:24 +0100

    gray background footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,26 @@
-
+import style from "./Footer.module.css"
 
 function Footer() {
     return (
         <>
-            <div>Footer</div>
+            <div className={style.background}>
+                <div className={style.icone}>
+                    Icone varie
+                </div>
+            </div>
+
+            <div className={style.graybackground}>
+                <div className={style.boxed}>
+                    <div className={style.liste}>
+                        liste
+                    </div>
+                    <div className={style.logodc}>
+                        Immagine DC in trasparenza
+                    </div>
+                </div>
+            </div>
+            <div>FollowUs</div>
+
         </>
     )
 }
