cadfbca5207d079e96e8467a85b701aa9c347719
federicosimone <federico.simone15@gmail.com>
2025-12-08 11:52:12 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,16 @@
 import style from "./Footer.module.css"
+import fb from "../assets/footer-facebook.png"
 
 function Footer() {
     return (
         <>
             <div className={style.background}>
-                <div className={style.icone}>
+                <div className={style.boxed}>
                     Icone varie
                 </div>
             </div>
 
-            <div className={style.graybackground}>
+            <div className={style.comicsbackground}>
                 <div className={style.boxed}>
                     <div className={style.liste}>
                         liste
@@ -19,7 +20,17 @@ function Footer() {
                     </div>
                 </div>
             </div>
-            <div>FollowUs</div>
+            <div className={style.graybackground}>
+                <div className={`${style.boxed} ${style.followus}`}>
+                    <div>
+                        <button>SIGN-OP NOW!</button>
+                    </div>
+                    <div>
+                        <span>FOLLOW US</span>
+                        <a href="#"><img src={fb} alt="fb" className={style.socialIcons} /></a>
+                    </div>
+                </div>
+            </div>
 
         </>
     )
