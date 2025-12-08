a2e694605428b02daa89a7d82d3e159b70adebd8
federicosimone <federico.simone15@gmail.com>
2025-12-08 10:11:19 +0100

    navbar semi-ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,24 +5,24 @@ export default function Header() {
     return (
         <>
             <header>
-
-
-                <img src={logo} alt="logo" />
-
-
                 <nav>
-                    <ul>
-                        <li><a href="#">CHARACTERS</a></li>
-                        <li><a href="#">COMICS</a></li>
-                        <li><a href="#">MOVIES</a></li>
-                        <li><a href="#">TV</a></li>
-                        <li><a href="#">GAMES</a></li>
-                        <li><a href="#">COLLECTIBLES</a></li>
-                        <li><a href="#">VIDEOS</a></li>
-                        <li><a href="#">FANS</a></li>
-                        <li><a href="#">NEWS</a></li>
-                        <li><a href="#">SHOP</a></li>
-                    </ul>
+                    <div id="logo">
+                        <img src={logo} alt="logo" />
+                    </div>
+                    <div>
+                        <ul>
+                            <li><a href="#">CHARACTERS</a></li>
+                            <li><a href="#">COMICS</a></li>
+                            <li><a href="#">MOVIES</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">GAMES</a></li>
+                            <li><a href="#">COLLECTIBLES</a></li>
+                            <li><a href="#">VIDEOS</a></li>
+                            <li><a href="#">FANS</a></li>
+                            <li><a href="#">NEWS</a></li>
+                            <li><a href="#">SHOP</a></li>
+                        </ul>
+                    </div>
                 </nav>
             </header>
         </>
