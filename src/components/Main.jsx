70b4526ef6c6c23ad331460cd321a427db9b300b
federicosimone <federico.simone15@gmail.com>
2025-12-14 12:28:27 +0100

    bg jumbotron

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,19 @@
 import style from "./Main.module.css";
 import Ecommerce from "./Ecommerce";
+import bgImage from "../assets/jumbotron.jpg"
 
 
 function Main() {
     return (
         <>
             <main>
-                <div className={style.container}>--Content goes Here--</div>
+                <div className={style.jumbotron}>
+                    <div className={style.container}>--Content goes Here--</div>
+
+
+                </div>
+
+
 
                 <Ecommerce />
 
